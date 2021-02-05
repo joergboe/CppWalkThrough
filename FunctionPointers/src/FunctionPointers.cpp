64d64ab4eaaee459d295044198e02adab2d8ddb4
joergboe <joergboe@de.ibm.com>
2021-02-05 17:48:57 +0100

    Add FunctionPointers

--- FunctionPointers/src/FunctionPointers.cpp
+++ FunctionPointers/src/FunctionPointers.cpp
@@ -29,15 +29,21 @@ int main() {
 	fp1 = f1;
 	cout << "fp1(4) = " << fp1(4) << endl;
 	cout << "The function assigned to the pointer must have the same number and type of parameters and return type\n";
-	cout << "noexcept specification is part of the function type. A normal pointer may point to a noexcept funtion\n";
+	cout << "noexcept specification is part of the function type.\n"
+			"A normal function pointer may point to a noexcept function\n";
 	int (* fp1ne)(int) noexcept = &f1ne;
 	res = (*fp1ne)(4);
 	cout << "res=" << res << endl;
-	cout << "A noexcept pointer must not point to a normal function (which may throw an execpion)\n";
+	cout << "But a noexcept pointer must not point to a normal function (which may throw an execpion)\n";
 	fp1ne = &f1;
-	cout << "This may be a problem with (GCC) 4.8.5\n";
-	res = (*fp1ne)(4);
-	cout << "res=" << res << endl;
+	cout << "This may be a problem with (GCC) 4.8.5\n"
+			"The program may be terminated even if an exception handler is there! (due to violation of the nothrow promise)\n";
+	try {
+		res = (*fp1ne)(4);
+		cout << "res=" << res << endl;
+	} catch (exception const & e) {
+		cout << "exception caught: " << e.what() << endl;
+	}
 
 	//fp1 = &f2; //error: invalid conversion from ‘int* (*)(int)’ to ‘int (*)(int)’ [-fpermissive]
 	//fp1 = &f3; //dito error
@@ -59,29 +65,29 @@ int main() {
 	int const & (*fp5)(int inp) = &f5;
 	cout << "resfp5=" << (*fp5)(4) << endl;
 
-	cout << "Use type alias for function pointer old stype\n";
+	cout << "Use type alias for function pointer old style\n";
 	typedef int (*Palias1)(int);
 	Palias1 pa1 = &f1;
 	cout << "(*pa1)(2)=" << (*pa1)(2) << endl;
 
-	cout << "Use type alias for function type old stype and use it as pointer\n";
+	cout << "Use type alias for function type old style and use it as pointer\n";
 	typedef int Alias1(int);
 	Alias1 * pa2 = &f1;
 	cout << "(*pa2)(2)=" << (*pa2)(2) << endl;
 
-	cout << "Use type alias for function pointer new stype\n";
+	cout << "Use type alias for function pointer new style\n";
 	using Palias2 = int(*)(int);
 	Palias2 pa3 = f1;
 	cout << "pa3(3)=" << pa3(3) << endl;
 
-	cout << "Use type alias for function type and use it as pointer new stype\n";
+	cout << "Use type alias for function type and use it as pointer new style\n";
 	using Alias2 = int (int);
 	Alias2 * pa4 = f1;
 	cout << "pa4(3)=" << pa4(3) << endl;
 
 	cout << "Exception specification should not be part of the alias. Problem with (GCC) 4.8.5\n";
 	using Alias3 = int(*)(int) noexcept;
-	cout << "This assignement shjould not be possible. Problem with (GCC) 4.8.5\n";
+	cout << "This assignment should not be possible. Problem with (GCC) 4.8.5\n";
 	Alias3 pa5 = &f1;
 	cout << "(*pa5)(3)=" << (*pa5)(3) << endl;
 
@@ -94,6 +100,7 @@ int main() {
 	cout << "typeinfo Palias2 : " << typeid(Palias2).name() << endl;
 	cout << "typeinfo Alias1  : " << typeid(Alias1).name() << endl;
 	cout << "typeinfo Alias2  : " << typeid(Alias2).name() << endl;
+	cout << "typeinfo Alias3  : " << typeid(Alias3).name() << endl;
 
 	cout << "END" << endl;
 	return 0;
@@ -109,17 +116,21 @@ int f1ne(int inp) noexcept {
 }
 int * f2(int inp) {
 	resf2 = inp + inp + 2;
+	//Never return a reference to local variables in a real program. This is only for the sake of demonstration.
 	return &resf2;
 }
 int const * f3(int inp) {
 	resf3 = inp + inp + 3;
+	//Never return a reference to local variables in a real program. This is only for the sake of demonstration.
 	return &resf3;
 }
 int & f4(int inp) {
 	resf4 = inp + inp + 4;
+	//Never return a reference to local variables in a real program. This is only for the sake of demonstration.
 	return resf4;
 }
 int const & f5(int inp) {
 	resf5 = inp + inp + 5;
+	//Never return a reference to local variables in a real program. This is only for the sake of demonstration.
 	return resf5;
 }
