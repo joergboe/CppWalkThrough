35d5e470629eb2cf9d2f93de1f0c3d1267dc8709
joergboe <joergboe@de.ibm.com>
2021-04-13 12:49:55 +0200

    Correction

--- ModulesLink/src/Module3.cpp
+++ ModulesLink/src/Module3.cpp
@@ -2,7 +2,7 @@
  * Module3.cpp
  */
 
-#include "../incl/Module2.hpp"
+#include "../incl/Module3.hpp"
 
 // It is not guaranteed that the linker can detect duplicates
 //int i2 = 20; // multiple definition of `i2'
